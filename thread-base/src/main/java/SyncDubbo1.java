6399c7ab2aaa9b48acda5393349d9957e9c855f1
janita <zcj880902>
2017-09-21 17:22:36 +0800

    thread

--- thread-base/src/main/java/SyncDubbo1.java
+++ thread-base/src/main/java/SyncDubbo1.java
@@ -5,26 +5,21 @@
  */
 public class SyncDubbo1 {
 
-	public synchronized void method1(){
+	private synchronized void method1(){
 		System.out.println("method1..");
 		method2();
 	}
-	public synchronized void method2(){
+	private synchronized void method2(){
 		System.out.println("method2..");
 		method3();
 	}
-	public synchronized void method3(){
+	private synchronized void method3(){
 		System.out.println("method3..");
 	}
 	
 	public static void main(String[] args) {
 		final SyncDubbo1 sd = new SyncDubbo1();
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				sd.method1();
-			}
-		});
+		Thread t1 = new Thread(sd::method1);
 		t1.start();
 	}
 }
