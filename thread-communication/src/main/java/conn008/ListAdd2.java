1b4ba267a7cc47d15d447bb889a0725156d777cc
janita <zcj880902>
2017-09-22 11:23:13 +0800

    thread-communication

--- thread-communication/src/main/java/conn008/ListAdd2.java
+++ thread-communication/src/main/java/conn008/ListAdd2.java
@@ -28,54 +28,49 @@ public class ListAdd2 {
 		// 1 实例化出来一个 lock
 		// 当使用wait 和 notify 的时候 ， 一定要配合着synchronized关键字去使用
 		//final Object lock = new Object();
-		
-		final CountDownLatch countDownLatch = new CountDownLatch(1);
-		
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					//synchronized (lock) {
-						for(int i = 0; i <10; i++){
-							list2.add();
-							System.out.println("当前线程：" + Thread.currentThread().getName() + "添加了一个元素..");
-							Thread.sleep(500);
-							if(list2.size() == 5){
-								System.out.println("已经发出通知..");
-								countDownLatch.countDown();
-								//lock.notify();
-							}
-						}						
-					//}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
 
-			}
-		}, "t1");
+		//需要多少次通知才能唤醒
+		final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+		Thread t1 = new Thread(()-> {
+            try {
+                //synchronized (lock) {
+                    for(int i = 0; i <10; i++){
+                        list2.add();
+                        System.out.println("当前线程：" + Thread.currentThread().getName() + "添加了一个元素..");
+                        Thread.sleep(500);
+                        if(list2.size() == 5){
+                            System.out.println("已经发出通知..");
+                            countDownLatch.countDown();
+                            countDownLatch.countDown();
+                            //lock.notify();
+                        }
+                    }
+                //}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 		
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				//synchronized (lock) {
-					if(list2.size() != 5){
-						try {
-							//System.out.println("t2进入...");
-							//lock.wait();
-							countDownLatch.await();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					System.out.println("当前线程：" + Thread.currentThread().getName() + "收到通知线程停止..");
-					throw new RuntimeException();
-				//}
-			}
-		}, "t2");	
-		
+		Thread t2 = new Thread(() -> {
+            //synchronized (lock) {
+                if(list2.size() != 5){
+                    try {
+                        //System.out.println("t2进入...");
+                        //lock.wait();
+                        countDownLatch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("当前线程：" + Thread.currentThread().getName() + "收到通知线程停止..");
+                throw new RuntimeException();
+            //}
+        }, "t2");
+
 		t2.start();
 		t1.start();
-		
+
 	}
-	
+
 }
