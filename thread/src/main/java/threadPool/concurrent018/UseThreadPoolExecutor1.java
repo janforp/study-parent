2f7f164fc8e7749b2c6fa7df6fed7c42bc226791
janita <zcj880902>
2017-09-25 17:31:43 +0800

    ThreadPool

--- thread/src/main/java/threadPool/concurrent018/UseThreadPoolExecutor1.java
+++ thread/src/main/java/threadPool/concurrent018/UseThreadPoolExecutor1.java
@@ -20,19 +20,18 @@ public class UseThreadPoolExecutor1 {
 		 * 若大于corePoolSize，则会将任务加入队列，
 		 * 若队列已满，则在总线程数不大于maximumPoolSize的前提下，创建新的线程，
 		 * 若线程数大于maximumPoolSize，则执行拒绝策略。或其他自定义方式。
-		 * 
-		 */	
+		 */
 		ThreadPoolExecutor pool = new ThreadPoolExecutor(
 				1, 				//coreSize
 				2, 				//MaxSize
-				60, 			//60
+				60, 			//60s之后释放
 				TimeUnit.SECONDS, 
-				new ArrayBlockingQueue<Runnable>(3)			//指定一种队列 （有界队列）
+				new ArrayBlockingQueue<>(3)			//指定一种队列 （有界队列）
 				//new LinkedBlockingQueue<Runnable>()
-				, new MyRejected()
+//				, new MyRejected()
 				//, new DiscardOldestPolicy()
 				);
-		
+
 		MyTask mt1 = new MyTask(1, "任务1");
 		MyTask mt2 = new MyTask(2, "任务2");
 		MyTask mt3 = new MyTask(3, "任务3");
