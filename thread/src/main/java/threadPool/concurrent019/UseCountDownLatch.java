09db14680dc611047d5e60b5cdcca2dab4922727
janita <zcj880902>
2017-09-26 10:50:26 +0800

    pool

--- thread/src/main/java/threadPool/concurrent019/UseCountDownLatch.java
+++ thread/src/main/java/threadPool/concurrent019/UseCountDownLatch.java
@@ -1,58 +1,53 @@
 package threadPool.concurrent019;
-
 import java.util.concurrent.CountDownLatch;
-
 public class UseCountDownLatch {
 
+	/**
+	 * 使用场景:
+	 * 正如每个Java文档所描述的那样，CountDownLatch是一个同步工具类，它允许一个或多个线程一直等待，直到其他线程的操作执行完后再执行
+	 * 例如，应用程序的主线程希望在负责启动框架服务的线程已经启动所有的框架服务之后再执行
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+		/**
+		 * 参数2：表示要得到几次通知才能唤醒等待的线程
+		 */
 		final CountDownLatch countDown = new CountDownLatch(2);
-		
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					System.out.println("进入线程t1" + "等待其他线程处理完成...");
-					countDown.await();
+		Thread t1 = new Thread(() -> {
+            try {
+                System.out.println("进入线程t1" + "等待其他线程处理完成...");
+                countDown.await();//在此阻塞，通过之后才往下执行
+				while (true) {
+					Thread.sleep(1000);
 					System.out.println("t1线程继续执行...");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
 				}
-			}
-		},"t1");
+			} catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"t1");
 		
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					System.out.println("t2线程进行初始化操作...");
-					Thread.sleep(3000);
-					System.out.println("t2线程初始化完毕，通知t1线程继续...");
-					countDown.countDown();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		Thread t3 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					System.out.println("t3线程进行初始化操作...");
-					Thread.sleep(4000);
-					System.out.println("t3线程初始化完毕，通知t1线程继续...");
-					countDown.countDown();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
+		Thread t2 = new Thread(() -> {
+            try {
+                System.out.println("t2线程进行初始化操作...");
+                Thread.sleep(3000);
+                System.out.println("t2线程初始化完毕，通知t1线程继续...");
+                countDown.countDown();//一个
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+		Thread t3 = new Thread(() -> {
+            try {
+                System.out.println("t3线程进行初始化操作...");
+                Thread.sleep(4000);
+                System.out.println("t3线程初始化完毕，通知t1线程继续...");
+                countDown.countDown();//2个
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 		t1.start();
 		t2.start();
 		t3.start();
-		
-		
-		
 	}
 }
--- thread/src/main/java/threadPool/concurrent019/UseCyclicBarrier.java
+++ thread/src/main/java/threadPool/concurrent019/UseCyclicBarrier.java
@@ -7,11 +7,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors; 
 public class UseCyclicBarrier {
 
-	static class Runner implements Runnable {  
+	static class Runner implements Runnable {
 	    private CyclicBarrier barrier;  
 	    private String name;  
 	    
-	    public Runner(CyclicBarrier barrier, String name) {  
+	    Runner(CyclicBarrier barrier, String name) {
 	        this.barrier = barrier;  
 	        this.name = name;  
 	    }  
@@ -19,21 +19,21 @@ public class UseCyclicBarrier {
 	    public void run() {  
 	        try {  
 	            Thread.sleep(1000 * (new Random()).nextInt(5));  
-	            System.out.println(name + " 准备OK.");  
+	            System.out.println(name + " 准备OK.");
+	            //所有运动员都等待
 	            barrier.await();  
-	        } catch (InterruptedException e) {  
+	        } catch (InterruptedException | BrokenBarrierException e) {
 	            e.printStackTrace();  
-	        } catch (BrokenBarrierException e) {  
-	            e.printStackTrace();  
-	        }  
-	        System.out.println(name + " Go!!");  
+	        }
+			System.out.println(name + " --- Go!!");
 	    }  
 	} 
 	
-    public static void main(String[] args) throws IOException, InterruptedException {  
-        CyclicBarrier barrier = new CyclicBarrier(3);  // 3 
+    public static void main(String[] args) throws IOException, InterruptedException {
+		//表示3个运动员（线程数）
+        CyclicBarrier barrier = new CyclicBarrier(3);  // 3
         ExecutorService executor = Executors.newFixedThreadPool(3);  
-        
+        //3个使用同一个 barrier,只要3个人都调用了 该 barrier 的 barrier.await();  方法，则都会继续往下执行
         executor.submit(new Thread(new Runner(barrier, "zhangsan")));  
         executor.submit(new Thread(new Runner(barrier, "lisi")));  
         executor.submit(new Thread(new Runner(barrier, "wangwu")));  
