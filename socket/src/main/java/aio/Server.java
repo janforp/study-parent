af617a5e8801d0a08b78ff579a717d730a02f8d2
janita <zcj880902>
2017-09-28 15:55:25 +0800

    nio

--- socket/src/main/java/aio/Server.java
+++ socket/src/main/java/aio/Server.java
@@ -6,33 +6,26 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/**
- */
 public class Server {
-	//线程池
-	private ExecutorService executorService;
-	//线程组
-	private AsynchronousChannelGroup threadGroup;
 	//服务器通道
-	public AsynchronousServerSocketChannel assc;
+	AsynchronousServerSocketChannel channel;
 	
-	public Server(int port){
+	private Server(int port){
 		try {
 			//创建一个缓存池
-			executorService = Executors.newCachedThreadPool();
+			ExecutorService executorService = Executors.newCachedThreadPool();
 			//创建线程组
-			threadGroup = AsynchronousChannelGroup.withCachedThreadPool(executorService, 1);
+			AsynchronousChannelGroup threadGroup = AsynchronousChannelGroup.withCachedThreadPool(executorService, 1);
 			//创建服务器通道
-			assc = AsynchronousServerSocketChannel.open(threadGroup);
+			channel = AsynchronousServerSocketChannel.open(threadGroup);
 			//进行绑定
-			assc.bind(new InetSocketAddress(port));
+			channel.bind(new InetSocketAddress(port));
 			
 			System.out.println("server start , port : " + port);
 			//进行阻塞
-			assc.accept(this, new ServerCompletionHandler());
+			channel.accept(this, new ServerCompletionHandler());
 			//一直阻塞 不让服务器停止
 			Thread.sleep(Integer.MAX_VALUE);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,5 +34,4 @@ public class Server {
 	public static void main(String[] args) {
 		Server server = new Server(8765);
 	}
-	
 }
