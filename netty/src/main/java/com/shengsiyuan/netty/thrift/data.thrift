72177eca0b5eb4b5aa530b63ac013bef863f657e
zhucj <zhucj@servyou.com.cn>
2020-04-14 20:24:56 +0800

    thrift

--- netty/src/main/java/com/shengsiyuan/netty/thrift/data.thrift
+++ netty/src/main/java/com/shengsiyuan/netty/thrift/data.thrift
@@ -1,4 +1,7 @@
+#生成java类的包
 namespace java thrift.generated
+
+#文件包含， include "global.thrift"
 
 typedef i16 short
 typedef i32 int
@@ -6,21 +9,30 @@ typedef i64 long
 typedef bool boolean
 typedef string String
 
-//thrift --gen java src/java/thrift/data.thrift
+# const int DEFAULT = 0
+
+#thrift --gen java src/java/thrift/data.thrift
 
 struct Person {
+
     1:optional String username,
+
     2:optional int age,
+
     3:optional boolean married
 }
 
 exception DataException {
+
     1:optional String message,
+
     2:optional  String callStack,
+
     3:optional String date
 }
 
 service PersonService {
+
     Person getPersonByUsername(1:required String username) throws(1:DataException dataException),
 
     void savePerson(1:required Person person) throws(1:DataException dataException)
