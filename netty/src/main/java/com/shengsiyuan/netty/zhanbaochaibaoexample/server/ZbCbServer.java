37b7645c33e6b8a0318fc2c44838ed64002f1b99
zhucj <zhucj@servyou.com.cn>
2020-04-15 20:04:35 +0800

    注释该行就会出现 TCP 粘包/拆包

--- netty/src/main/java/com/shengsiyuan/netty/zhanbaochaibaoexample/server/ZbCbServer.java
+++ netty/src/main/java/com/shengsiyuan/netty/zhanbaochaibaoexample/server/ZbCbServer.java
@@ -17,15 +17,18 @@ public class ZbCbServer {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //设置并绑定Reactor线程池：EventLoopGroup，EventLoop就是处理所有注册到本线程的Selector上面的Channel
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup work = new NioEventLoopGroup();
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap
                     .group(boss, work)
+                    //设置并绑定服务端的channel
                     .channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
                     .childHandler(new ZbCbServerInitializer());
+            //当轮训到准备就绪的channel后，由Reactor线程：NioEventLoop执行pipline中的方法，最终调度并执行channelHandler
             ChannelFuture channelFuture = bootstrap.bind(8899).sync();
             channelFuture.channel().closeFuture().sync();
         } finally {
