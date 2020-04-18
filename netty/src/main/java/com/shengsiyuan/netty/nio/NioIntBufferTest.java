d3c2c650628f35b7c2ae834bc5244903515ebbba
zhucj <zhucj@servyou.com.cn>
2020-04-18 18:08:16 +0800

    nio

--- netty/src/main/java/com/shengsiyuan/netty/nio/NioIntBufferTest.java
+++ netty/src/main/java/com/shengsiyuan/netty/nio/NioIntBufferTest.java
@@ -12,6 +12,15 @@ import java.security.SecureRandom;
 public class NioIntBufferTest {
 
     public static void main(String[] args) {
+
+        testOne();
+
+        System.out.println("分割分割分割分割分割分割分割分割分割分割分割分割分割分割分割分割分割");
+
+        testTwo();
+    }
+
+    private static void testOne() {
         IntBuffer buffer = IntBuffer.allocate(10);
         for (int i = 0; i < buffer.capacity(); i++) {
             int randomNumber = new SecureRandom().nextInt(20);
@@ -25,6 +34,31 @@ public class NioIntBufferTest {
 
         //读取
         while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
+    }
+
+    private static void testTwo() {
+        IntBuffer buffer = IntBuffer.allocate(10);
+        for (int i = 0; i < 5; i++) {
+            int randomNumber = new SecureRandom().nextInt(20);
+            //写入
+            buffer.put(randomNumber);
+        }
+
+        //10
+        System.out.println("before flip limit = " + buffer.limit());
+        //翻转
+        buffer.flip();
+        //5
+        System.out.println("after flip limit = " + buffer.limit());
+
+
+        //读取
+        while (buffer.hasRemaining()) {
+            System.out.println("position = " + buffer.position());
+            System.out.println("limit = " + buffer.limit());
+            System.out.println("capacity = " + buffer.capacity());
             System.out.println(buffer.get());
         }
     }
