c8525f31813de62bdfd2b04e48f73ae23d394ae4
zhucj <zhucj@servyou.com.cn>
2020-04-19 10:26:33 +0800

    SelectorTest

--- netty/src/main/java/com/shengsiyuan/netty/nio/selector/SelectorTest.java
+++ netty/src/main/java/com/shengsiyuan/netty/nio/selector/SelectorTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -24,82 +23,6 @@ public class SelectorTest {
         testOne();
     }
 
-    private static void testOne() throws IOException {
-
-        int[] ports = getPorts();
-        Selector selector = Selector.open();
-        for (int i = 0; i < ports.length; i++) {
-
-            ServerSocketChannel serverSocketChannel
-                    = ServerSocketChannel.open();
-
-            serverSocketChannel.configureBlocking(false);
-
-            ServerSocket serverSocket = serverSocketChannel.socket();
-
-            InetSocketAddress inetSocketAddress
-                    = new InetSocketAddress(ports[i]);
-
-            serverSocket.bind(inetSocketAddress);
-
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-            System.out.println("监听端口："+ ports[i]);
-        }
-
-        while (true) {
-            int numbers = selector.select();
-            System.out.println("numbers = " + numbers);
-
-            Set<SelectionKey> selectionKeySet
-                    = selector.selectedKeys();
-
-            System.out.println("selectionKeySet : " + selectionKeySet);
-
-            Iterator<SelectionKey> iterator
-                    = selectionKeySet.iterator();
-
-            while (iterator.hasNext()) {
-                SelectionKey selectionKey = iterator.next();
-                if (selectionKey.isAcceptable()) {
-                    ServerSocketChannel channel
-                            = (ServerSocketChannel)selectionKey.channel();
-
-                    SocketChannel socketChannel = channel.accept();
-                    socketChannel.configureBlocking(false);
-
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-
-                    iterator.remove();
-
-                    System.out.println("获得客户端连接：" + socketChannel);
-
-                } else if (selectionKey.isReadable()) {
-                    SocketChannel socketChannel
-                            = (SocketChannel)selectionKey.channel();
-
-                    int bytesRead = 0;
-                    while (true) {
-                        ByteBuffer buffer = ByteBuffer.allocate(512);
-                        buffer.clear();
-
-                        int read = socketChannel.read(buffer);
-                        if (read <= 0) {
-                            break;
-                        }
-                        buffer.flip();
-
-                        socketChannel.write(buffer);
-                        bytesRead += read;
-                    }
-                    System.out.println("读取：" + bytesRead +" ,  来自于 :" + socketChannel);
-
-                    iterator.remove();
-                }
-            }
-        }
-    }
-
     private static int[] getPorts() {
         int[] ports = new int[5];
         ports[0] = 5000;
@@ -108,5 +31,66 @@ public class SelectorTest {
         ports[3] = 5003;
         ports[4] = 5004;
         return ports;
+    }
+
+    private static Selector initSelector(int[] ports) throws IOException {
+        Selector selector = Selector.open();
+        for (int port : ports) {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+            serverSocket.bind(inetSocketAddress);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("服务器监听端口：" + port);
+        }
+        return selector;
+    }
+
+    private static void doAccept(Selector selector, SelectionKey selectionKey, Iterator<SelectionKey> iterator) throws IOException {
+        ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();
+        SocketChannel socketChannel = channel.accept();
+        socketChannel.configureBlocking(false);
+        socketChannel.register(selector, SelectionKey.OP_READ);
+        iterator.remove();
+        System.out.println("获得客户端连接：" + socketChannel);
+    }
+
+    private static void doRead(Selector selector, SelectionKey selectionKey, Iterator<SelectionKey> iterator) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        int bytesRead = 0;
+        while (true) {
+            ByteBuffer buffer = ByteBuffer.allocate(512);
+            buffer.clear();
+            int read = socketChannel.read(buffer);
+            if (read <= 0) {
+                break;
+            }
+            buffer.flip();
+            socketChannel.write(buffer);
+            bytesRead += read;
+        }
+        System.out.println("读取：" + bytesRead + " ,  来自于 :" + socketChannel);
+        iterator.remove();
+    }
+
+    private static void testOne() throws IOException {
+        int[] ports = getPorts();
+        Selector selector = initSelector(ports);
+        while (true) {
+            int numbers = selector.select();
+            System.out.println("selector.select() 的结果 numbers = " + numbers);
+            Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+            System.out.println("selectionKeySet : " + selectionKeySet);
+            Iterator<SelectionKey> iterator = selectionKeySet.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                if (selectionKey.isAcceptable()) {
+                    doAccept(selector, selectionKey, iterator);
+                } else if (selectionKey.isReadable()) {
+                    doRead(selector, selectionKey, iterator);
+                }
+            }
+        }
     }
 }
