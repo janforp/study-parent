7ec3d43b8d262c1ae746cbff4bd4badf654bbb7c
zhucj <zhucj@servyou.com.cn>
2020-05-23 16:08:32 +0800

    ToDoList

--- java-base/src/main/java/com/janita/java/base/thinkinjava/_17_container/QueueBehavior.java
+++ java-base/src/main/java/com/janita/java/base/thinkinjava/_17_container/QueueBehavior.java
@@ -53,11 +53,62 @@ public class QueueBehavior {
         test(new LinkedBlockingQueue<String>(), new Gen());
         test(new PriorityBlockingQueue<String>(), new Gen());
     }
-} /* Output:
-one two three four five six seven eight nine ten
-eight five four nine one seven six ten three two
-one two three four five six seven eight nine ten
-one two three four five six seven eight nine ten
-one two three four five six seven eight nine ten
-eight five four nine one seven six ten three two
-*///:~
+}
+
+class ToDoList extends PriorityQueue<ToDoList.ToDoItem> {
+
+    /**
+     * 添加一个元素到自身
+     */
+    public void add(String item, char primary, int secondary) {
+        ToDoItem toDoItem = new ToDoItem(item, primary, secondary);
+        super.add(toDoItem);
+    }
+
+    static class ToDoItem implements Comparable<ToDoItem> {
+
+        private char primary;
+
+        private int secondary;
+
+        private String item;
+
+        public ToDoItem(String td, char pri, int sec) {
+            primary = pri;
+            secondary = sec;
+            item = td;
+        }
+
+        public int compareTo(ToDoItem arg) {
+            if (primary > arg.primary) {
+                return +1;
+            }
+            if (primary == arg.primary) {
+                if (secondary > arg.secondary) {
+                    return +1;
+                } else if (secondary == arg.secondary) {
+                    return 0;
+                }
+            }
+            return -1;
+        }
+
+        @Override
+        public String toString() {
+            return Character.toString(primary) + secondary + ": " + item;
+        }
+    }
+
+    public static void main(String[] args) {
+        ToDoList toDoList = new ToDoList();
+        toDoList.add("Empty trash", 'C', 4);
+        toDoList.add("Feed dog", 'A', 2);
+        toDoList.add("Feed bird", 'B', 7);
+        toDoList.add("Mow lawn", 'C', 3);
+        toDoList.add("Water lawn", 'A', 1);
+        toDoList.add("Feed cat", 'B', 1);
+        while (!toDoList.isEmpty()) {
+            System.out.println(toDoList.remove());
+        }
+    }
+}
