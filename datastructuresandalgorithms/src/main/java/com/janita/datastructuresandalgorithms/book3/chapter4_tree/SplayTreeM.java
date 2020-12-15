a663b2b0742efdff318efe0dfdc6ad136dcd31ef
zhucj <zhucj@servyou.com.cn>
2020-12-15 20:49:23 +0800

    SplayTreeM

--- datastructuresandalgorithms/src/main/java/com/janita/datastructuresandalgorithms/book3/chapter4_tree/SplayTreeM.java
+++ datastructuresandalgorithms/src/main/java/com/janita/datastructuresandalgorithms/book3/chapter4_tree/SplayTreeM.java
@@ -28,7 +28,7 @@ public class SplayTreeM<T extends Comparable<? super T>> {
     }
 
     /**
-     * 试图找到e，如果e不存在，则找到接近
+     * 试图找到e，如果e不存在，则找到接近e的节点
      *
      * @param v
      * @param e
@@ -54,6 +54,25 @@ public class SplayTreeM<T extends Comparable<? super T>> {
             }
         }
         return result;
+    }
+
+    private T insert(T e) {
+        if (root == null) {
+            size++;
+            root = new SplayTreeNode<>(e);
+            return e;
+        }
+        SplayTreeNode<T> search = search(e);
+        if (search != null && search.element.equals(e)) {
+            return e;
+        }
+        size++;
+        @SuppressWarnings("unchecked")
+        SplayTreeNode<Integer> t = (SplayTreeNode<Integer>) root;
+        SplayTreeNode<T> newNode = new SplayTreeNode<>(e);
+        SplayTreeNode<Integer> left = t.left;
+        SplayTreeNode<Integer> right = t.right;
+
     }
 
     private SplayTreeNode<T> splay(SplayTreeNode<T> v) {
@@ -273,6 +292,87 @@ public class SplayTreeM<T extends Comparable<? super T>> {
 
         SplayTreeM<Integer> splay = new SplayTreeM<>();
         splay.splay(node1);
+        Assert.assertEquals(7, (int) node6.right.element);
+
+        splay.splay(node3);
+
+        Assert.assertEquals(1, (int) node3.left.element);
+        Assert.assertEquals(14, (int) node3.right.element);
+
+        Assert.assertEquals(6, (int) node10.left.element);
+        Assert.assertEquals(13, (int) node12.right.element);
+
+    }
+
+    @Test
+    public void test3() {
+        SplayTreeNode<Integer> root = new SplayTreeNode<>(15);
+        SplayTreeNode<Integer> node14 = new SplayTreeNode<>(14);
+        SplayTreeNode<Integer> node13 = new SplayTreeNode<>(13);
+        SplayTreeNode<Integer> node12 = new SplayTreeNode<>(12);
+        SplayTreeNode<Integer> node11 = new SplayTreeNode<>(11);
+        SplayTreeNode<Integer> node10 = new SplayTreeNode<>(10);
+        SplayTreeNode<Integer> node9 = new SplayTreeNode<>(9);
+        SplayTreeNode<Integer> node8 = new SplayTreeNode<>(8);
+
+        SplayTreeNode<Integer> node7 = new SplayTreeNode<>(7);
+        SplayTreeNode<Integer> node6 = new SplayTreeNode<>(6);
+        SplayTreeNode<Integer> node5 = new SplayTreeNode<>(5);
+        SplayTreeNode<Integer> node4 = new SplayTreeNode<>(4);
+        SplayTreeNode<Integer> node3 = new SplayTreeNode<>(3);
+        SplayTreeNode<Integer> node2 = new SplayTreeNode<>(2);
+        SplayTreeNode<Integer> node1 = new SplayTreeNode<>(1);
+
+        root.left = node14;
+        node14.parent = root;
+
+        node14.left = node13;
+        node13.parent = node14;
+
+        node13.left = node12;
+        node12.parent = node13;
+
+        node12.left = node11;
+        node11.parent = node12;
+
+        node11.left = node10;
+        node10.parent = node11;
+
+        node10.left = node9;
+        node9.parent = node10;
+
+        node9.left = node8;
+        node8.parent = node9;
+
+        node8.left = node7;
+        node7.parent = node8;
+
+        node7.left = node6;
+        node6.parent = node7;
+
+        node6.left = node5;
+        node5.parent = node6;
+
+        node5.left = node4;
+        node4.parent = node5;
+
+        node4.left = node3;
+        node3.parent = node4;
+
+        node3.left = node2;
+        node2.parent = node3;
+
+        node2.left = node1;
+        node1.parent = node2;
+
+        Assert.assertNotNull(root);
+
+        SplayTreeM<Integer> treeM = new SplayTreeM<>();
+        treeM.root = root;
+        SplayTreeNode<Integer> search = treeM.search(0);
+        Assert.assertNotNull(search);
+
+        SplayTreeM<Integer> splay = new SplayTreeM<>();
         Assert.assertEquals(7, (int) node6.right.element);
 
         splay.splay(node3);
